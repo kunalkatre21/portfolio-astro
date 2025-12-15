db40d559a8c6525a9c313d9de982c887e4c64dd3
shark-eating-cheesebuggah <160771239+kunalkatre21@users.noreply.github.com>
2025-12-15 18:14:56 +0530

    Add and commit all changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/portfolio-astro/',
+  site: 'https://kunalkatre21.github.io',
+  base: '/portfolio-astro',
   vite: {
     plugins: [tailwindcss()]
   }
